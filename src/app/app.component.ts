f1471b81d960827d0cdf48829618aebc56ecfc34
Akshit <Gakshit92@gmail.com>
2023-01-11 11:29:00 +0530

    Adding basic UI and form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'github-frontend';
+  userNameForm = new FormGroup({
+    userName: new FormControl(''),
+  });
+  handleSubmit() {
+    console.log(this.userNameForm.value);
+  }
 }
